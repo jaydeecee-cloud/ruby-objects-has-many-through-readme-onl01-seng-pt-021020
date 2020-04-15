54b0558e0f3535739eeb45de152645501f0990b7
Joseph Choi <jdc07@live.com>
2020-04-15 21:00:31 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,14 +16,4 @@ class Customer
   def new_meal(waiter, total, tip=0)
     Meal.new(waiter, self, total, tip)
   end
-
-  def meals
-  Meal.all.select do |meal|
-    meal.customer == self
-  end
-
-  def waiters
-  meals.map do |meal|
-    meal.waiter
-  end
 end
